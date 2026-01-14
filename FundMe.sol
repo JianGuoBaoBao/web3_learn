2b6471d107e1f431c932c8cbae48e94e422bdead
JianGuoBaoBao <791551390@qq.com>
2026-01-14 11:24:41 +0800

    day01-收款函数创建

---
+++ FundMe.sol
@@ -0,0 +1,68 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// 1. 创建一个收款函数
+// 2. 记录投资人并且查看
+// 3. 在锁定期内，达到目标值，生产商可以提款
+// 4. 在锁定期内，没有达到目标值，投资人在锁定期以后退款
+
+contract FundMe {
+    mapping(address => uint256) public fundersToAmount;
+
+    uint256 constant MINIMUM_VALUE = 100 * 10 ** 18; //USD
+    
+    AggregatorV3Interface internal dataFeed;
+
+    uint256 constant TARGET = 1000 * 10 ** 18; // 常量
+
+    address owner;
+
+
+    constructor() {
+        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
+
+    // msg.value 是一个 “全局变量”，它返回一个 uint256 类型的值，表示当前函数调用时，调用者随交易一同发送的 ETH 数量，其单位是 Wei。
+    function fund() external payable {
+        require(convertEthToUsd(msg.value) >= MINIMUM_VALUE, "Send more ETH");
+        fundersToAmount[msg.sender] = msg.value;
+    }
+
+    function getChainlinkDataFeedLatestAnswer() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
+
+    function convertEthToUsd(uint256 ethAmount) internal view returns(uint256){
+        uint256 ethPrice = uint256(getChainlinkDataFeedLatestAnswer());
+        return ethAmount * ethPrice / (10 ** 8);
+    }
+
+    function transferOwnership(address newOwner) public {
+        require(msg.sender == owner, "this function can only be called by owner");
+        owner = newOwner;
+    }
+
+    function getFund() external {
+        require(convertEthToUsd(address(this).balance) >= TARGET, "Target is not reached");
+        require(msg.sender == owner, "this functin can only be called by owner");
+
+        // transfer: transfer ETH and revert if tx failed
+        // payable(msg.sender).transfer(address(this).balance);
+
+         (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+         require(success, "Transfer failed");
+
+        // send
+        // call
+    }
+}
